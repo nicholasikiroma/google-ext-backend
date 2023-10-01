6907199ab9afb03b0584aed4aea3fdb61baf40b7
Nicholas <nicholasikiroma@gmail.com>
2023-10-02 00:03:14 +0100

    Increased limit JSON request body parsing

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ app.use(cors());
 app.use(morgan("dev"));
 
 // In-built middlewares
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb" }));
 
 // custom middlewares
 app.use("/api", router);
